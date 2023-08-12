32e93ecc9e7561a03960601d681bf428d49c72fa
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-08-12 15:49:01 -0700

    Storage

---
+++ models/engine/storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+import os.path
+import json
+
+
+class FileStorage:
+    """ Rep an abstracted storage engine.
+    Attributes:
+        __file_path (str): Name of the file to save objects to
+        __objects (dict): Dictionary of instantiated objects
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dict__objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
+    def save(self):
+        """Serialization of  __objects to the JSON-file filepath"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """Deserialization of the JSON file _file path to objects, if it exist"""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
