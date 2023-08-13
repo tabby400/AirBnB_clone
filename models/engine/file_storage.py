d906da5bc299ba3785238e6a45b557cb7825ac3f
tabby400 <tabbynargaret@gmail.com>
2023-08-13 00:51:12 -0700

    storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+import os.path
+import json
+
+
+class FileStorage:
+    """ this class represents the storage engine.
+    Attributes:
+        __file_path : the file to save objects in
+        __objects : Dict of instantiated objects
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """dict objects are returned"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """onjects with keys"""
+        jname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(jname, obj.id)] = obj
+
+    def save(self):
+        """objects are serialized to json"""
+        obdict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in obdict.keys()}
+        with open(FileStorage.__file_path, "w") as fh:
+            json.dump(objdict, fh)
+
+    def reload(self):
+        """if file exists,deserealize"""
+        try:
+            with open(FileStorage.__file_path) as fh:
+                objdict = json.load(fh)
+                for p in objdict.values():
+                    class_name = o["__class__"]
+                    del p["__class__"]
+                    self.new(eval(class_name)(**p))
+        except FileNotFoundError:
+            return
